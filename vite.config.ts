03a664ee4c1a50f12c5b298b9e3e2f8b6bf266f8
Quang Nguyen <quang.nguyen@omnicommediagroup.com>
2025-03-10 08:56:03 +0000

    add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
+import { configDefaults } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    globals: true,
+    environment: 'happy-dom'
+  }
 })

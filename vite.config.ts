63db9d22348aa5ad0bd2bc9cd0512fdb32698480
Quang Nguyen <quang.nguyen@omnicommediagroup.com>
2025-03-22 15:06:33 +0000

    feature: fix ts unable to resolve @ alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { configDefaults } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "url";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,5 +9,10 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'happy-dom'
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }  
   }
 })
